e8ab6fe27631304cbc1d5b5e96954e0504c64f4a
Smit <smithpatel895@gmail.com>
2023-03-21 23:44:45 +0530

    new 1

--- BSTInsertionhackerrank.cpp
+++ BSTInsertionhackerrank.cpp
@@ -1,1 +1,36 @@
 
+/* you only have to complete the function given below.
+Node is defined as
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+
+
+    Node * insert(Node * root, int data) {
+       if(root==NULL){
+           return new Node(data);
+       }
+
+                if(data<(root->data)){
+                    root->left=insert(root->left,data);
+                }
+                else{
+                     root->right=insert(root->right,data);
+                }
+
+
+
+        return root;
+    }
